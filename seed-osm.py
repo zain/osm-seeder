cdd49b4be3e9dd47c76048b40e36615c5548e0ac
Zain Memon <zain@inzain.net>
2011-04-24 23:58:17 -0700

    Second pass on the seed-osm script, this time using ?bbox instead of ?tile

--- seed-osm.py
+++ seed-osm.py
@@ -3,8 +3,8 @@ import httplib2, ModestMaps, os, sys, time
 API_URL = 'http://api.openstreetmap.org/api/0.6/map?bbox=%s'
 DELAY_SECS = 300
 MAX_TRIES = 3
-ZOOM_LEVEL = 14
-LOAD_COMMAND = 'osm2pgsql %s'
+ZOOM_LEVEL = 16
+LOAD_COMMAND = 'osm2pgsql --append --database=gis --username=postgres -W %s'
 
 
 class APIOfflineException(Exception):
@@ -13,12 +13,13 @@ class APIOfflineException(Exception):
 
 class OSMSeeder:
     def __init__(self, min_lat, min_lon, max_lat, max_lon):
-        mm = ModestMaps.OpenStreetMap.Provider()
+        self.mm = ModestMaps.OpenStreetMap.Provider()
         self.tiles = set()
 
         for lat, lon in [(lat, lon) for lat in [min_lat, max_lat] for lon in [min_lon, max_lon]]:
-            coord = mm.locationCoordinate(ModestMaps.Geo.Location(lat, lon)).zoomTo(ZOOM_LEVEL)
-            self.tiles.add((ZOOM_LEVEL, int(coord.column), int(coord.row)))
+            loc = ModestMaps.Geo.Location(lat, lon)
+            coord = self.mm.locationCoordinate(loc).zoomTo(ZOOM_LEVEL).container()
+            self.tiles.add(coord)
 
         self.log("Found %s tiles at zoom level %s." % (len(self.tiles), ZOOM_LEVEL))
 
@@ -33,20 +34,28 @@ class OSMSeeder:
             else:
                 first = False
 
-            content = self.fetch(self.url_for(*tile))
+            content = self.fetch(self.url_for(tile))
 
-            filename = "%s-%s-%s.osm" % tile
+            filename = "%s-%s-%s.osm" % (tile.zoom, tile.column, tile.row)
             f = open(filename, 'w+')
             f.write(content)
             f.close()
 
+            print "------------[Loading...]------------"
             os.system(LOAD_COMMAND % filename)
+            print "-------------[Finished]-------------"
 
-        self.log("Updated %s tiles in %ss." % (time.time() - start_time))
+        self.log("Updated in %ss." % (time.time() - start_time))
 
-    def url_for(self, z, x, y):
-        tile = "%s,%s,%s" % (z, x, y)
-        return API_URL % tile
+    def url_for(self, coord):
+        nw = self.mm.coordinateLocation(coord)
+        se = self.mm.coordinateLocation(coord.right().down())
+        max_lat = max(nw.lat, se.lat)
+        min_lat = min(nw.lat, se.lat)
+        max_lon = max(nw.lon, se.lon)
+        min_lon = min(nw.lon, se.lon)
+        bbox = "%s,%s,%s,%s" % (min_lon, min_lat, max_lon, max_lat)
+        return API_URL % bbox
 
     def fetch(self, url):
         self.log("- Fetching: %s" % url)
